e503470ffefce6fcc1ac8401a7ca67e3b60bdf92
Oladejo Toyyib <oladejotoyyibolalekan@gmail.com>
2023-01-10 13:00:54 +0100

    fifth commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,6 @@ const Home = () => {
   return (
     <div name='home' className='w-full h-screen bg-[#0a192f]'>
       
-
       {/* container */}
 
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
